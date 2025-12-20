7ae82f4f88e9a8a2628e315e8f995e0cdf78b45b
groomigroom <googjohn0143@gmail.com>
2025-12-20 18:25:09 +0900

    Add event listeners for box four and box five

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ let box_three_main_out = document.querySelector(".box_three_main_out");
 
 box_three.addEventListener("click", () => box_three_main.classList.add("on"));
 box_three_main_out.addEventListener("click", () => box_three_main.classList.remove("on"));
+
+/*box_four function*/
+
+let box_four = document.querySelector(".box_four");
+let box_four_main = document.querySelector(".box_four_main");
+let box_four_main_out = document.querySelector(".box_four_main_out");
+
+box_four.addEventListener("click", () => box_four_main.classList.add("on"));
+box_four_main_out.addEventListener("click", () => box_four_main.classList.remove("on"));
+
+/*box_five function*/
+
+let box_five = document.querySelector(".box_five");
+let box_five_main = document.querySelector(".box_five_main");
+let box_five_main_out = document.querySelector(".box_five_main_out");
+
+box_five.addEventListener("click", () => box_five_main.classList.add("on"));
+box_five_main_out.addEventListener("click", () => box_five_main.classList.remove("on"));
+
+
+
+
